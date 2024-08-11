3ed1a27c2841738b96875d7d14b44717df3f6071
rosekamau <kamaurose236@gmail.com>
2024-08-11 12:41:46 +0300

    react-tdd-events-codealong

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-function App() {
-  return (
-    <div></div>
-  )
-}
+import { useState } from "react";
 
-export default App;
+function App() {
+  const [pepperoniIsChecked, setPepperoniIsChecked] = useState(false);
+
+  function togglePepperoni(e) {
+    setPepperoniIsChecked(e.target.checked);
+  }
+
+  return (
+    <div>
+    <h1>Select Pizza Toppings</h1>
+    <input 
+      type="checkbox" 
+      id="pepperoni" 
+      checked={pepperoniIsChecked}
+      aria-checked={pepperoniIsChecked}
+      onChange={togglePepperoni}
+    />
+    <label htmlFor="pepperoni">Add pepperoni</label>
+
+    <h2>Your Toppings:</h2>
+    <ul>
+      <li>Cheese</li>
+      {pepperoniIsChecked ? <li>Pepperoni</li> : null}
+    </ul>
+
+  </div>
+)}
+
+export default App
